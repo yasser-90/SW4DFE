b51901d9e5917ffc48b81efc3c3435b9201ccc15
Aliwaleed22 <janedoe@gmail.comroot360rot>
2023-04-08 15:33:45 +0300

    Remove The Error

---
+++ text.py
@@ -0,0 +1,25 @@
+
+import time
+
+import tkinter
+
+import sin from math
+sum = 0
+
+
+li = [2,3,4,5,1]
+for i in li:
+    sum+=i
+print(sum) 
+
+'''
+ali mm
+'''
+
+
+'''
+ali mohameed taha\'''
+
+"""
+taha mohameed ali
+/"""
---
+++ texttext.py
@@ -0,0 +1,13 @@
+import timeCan Li
+
+import tkinterCan Li
+
+import sin from mathCan Li
+
+sum = 0
+li = [2,3,4,5,1]
+for i in li:
+sum+=i
+print(sum)
+ali mm
+ali mohameed taha\
